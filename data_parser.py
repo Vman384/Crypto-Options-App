5121f320f4a8588b137ee21721e84e07aa206859
Vman384 <112357168+Vman384@users.noreply.github.com>
2024-12-02 22:31:24 +1100

    cleaned up live option data display

--- data_parser.py
+++ data_parser.py
@@ -7,8 +7,10 @@ class DataParser:
     def __init__(self):
         self.optionSocketEndpoint = 'wss://nbstream.binance.com/eoptions/ws'
         self.token = "BTC@markPrice"
+        self.data_rows = 15
     
-    def set_token(self, token):
+    def set_token(self):
+        token = input("Specify token for which you would like data: ")
         self.token = token
 
     def get_live_option_data(self, id = 1):
@@ -22,7 +24,16 @@ class DataParser:
         def on_message(ws, message):
             out = json.loads(message)
             df = pd.DataFrame(out)
-            print(df)
+            df = df.tail(self.data_rows)
+            df.drop(columns=["e"])
+            df.drop(columns=["E"])
+            df_puts = df[df['s'].str.endswith('P')].reset_index(drop=True)
+            df_calls = df[df['s'].str.endswith('C')].reset_index(drop=True)
+
+            # Display the results
+            print("Puts:\n", df_puts)
+            print("\nCalls:\n", df_calls)
+
 
         ws = websocket.WebSocketApp(self.optionSocketEndpoint, on_message=on_message, on_open=on_open)
 
